d63455e2355178222052385fcf63f67d7301dfb8
alx-r-dev <alxrobey@icloud.com>
2025-12-13 08:50:28 -0500

    home page layout updates and adding icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,30 @@
+import { FaRunning } from "react-icons/fa";
 import styles from "./Header.module.css";
+import { MdArrowOutward } from "react-icons/md";
 
 const Header = () => {
   return (
-    <header>
-      <h1 className={styles.header}>
-        The <span className={styles.header__span}>Norwegian</span> Singles
-        Method add icon
+    <header className={styles.header}>
+      {<FaRunning size="50px" />}
+      <h1 className={styles.header__title__container}>
+        The <span className={styles.header__title}>Norwegian Singles</span>{" "}
+        Method
       </h1>
-      <button className={styles.header__button}>How it works icon</button>
+      <h6 className={styles.header__title__subtext}>
+        The sub-threshold training methodology taking the running world by storm
+        that took one runner from a 28 minute to a 15 minute 5k.
+      </h6>
+      <div className={styles.header__button__container}>
+        <button className={styles.header__button__get__started}>
+          Get Started
+        </button>
+        <button className={styles.header__button__how__it__works}>
+          How it Works{" "}
+          <span style={{ marginLeft: "5px" }}>
+            <MdArrowOutward />
+          </span>
+        </button>
+      </div>
     </header>
   );
 };
--- src/components/Vdot/CalculateVdot.tsx
+++ src/components/Vdot/CalculateVdot.tsx
@@ -1,34 +1,64 @@
+import { HiQuestionMarkCircle } from "react-icons/hi";
 import styles from "./CalculateVdot.module.css";
+import { useState } from "react";
 
 const CalculateVdot = () => {
+  const [recentRaceTime, setRecentRaceTime] = useState<string>("");
+  const [recentRaceDistance, setRecentRaceDistance] = useState<string>("mile");
+  const handleRecentRaceTimeInput = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRecentRaceTime(e.target.value);
+  };
+  const handleRecentRaceDistanceInput = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setRecentRaceDistance(e.target.value);
+  };
+  const handleVdotFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className={styles.vdot}>
       <div className={styles.vdot__title}>
-        Calculate VDOT add question mark thing with popup
+        Calculate VDOT {<HiQuestionMarkCircle />}
       </div>
       <div className={styles.vdot__subtext}>
         A running performance score to gauge current fitness level
       </div>
-      <form className={styles.vdot__form}>
+      <form onSubmit={handleVdotFormSubmit} className={styles.vdot__form}>
         <div className={styles.vdot__input_flexbox}>
-          <label className={styles.vdot__input}>
+          <label className={styles.vdot__input__label}>
             Recent Race Time
-            <input name="recent-race-time" placeholder="00:00:00" />
+            <input
+              className={styles.vdot__input__race}
+              name="recent-race-time"
+              onChange={handleRecentRaceTimeInput}
+              value={recentRaceTime}
+              placeholder="00:00:00"
+            />
           </label>
-          <label className={styles.vdot__input}>
+          <label className={styles.vdot__input__label}>
             Distance
-            <select name="recent-race-distance">
+            <select
+              value={recentRaceDistance}
+              className={styles.vdot__input__distance}
+              name="recent-race-distance"
+              onChange={handleRecentRaceDistanceInput}
+            >
               <option value="mile">Mile</option>
               <option value="2mile">2 Mile</option>
               <option value="5k">5k</option>
               <option value="10k">10k</option>
+              <option value="15k">15k</option>
               <option value="half">Half Marathon</option>
               <option value="Marathon">Marathon</option>
             </select>
           </label>
         </div>
         <button className={styles.vdot__calculate__button} type="submit">
-          Calculate
+          Analyze Race
         </button>
       </form>
     </section>
