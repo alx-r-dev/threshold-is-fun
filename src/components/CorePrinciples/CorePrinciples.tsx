9188f32de27bfac384485155833d7b4f83c650d3
alx-r-dev <alxrobey@icloud.com>
2025-12-31 09:44:23 -0500

    fix typos

--- src/components/CorePrinciples/CorePrinciples.tsx
+++ src/components/CorePrinciples/CorePrinciples.tsx
@@ -2,8 +2,8 @@ import styles from "./CorePrinciples.module.css";
 
 const subThresholdContent = `The bulk of your quality work is done below your lactate threshold(LT2). 
 This is generally between 2.5-3.5 mmol/L, allowing for high volume without burnout.`;
-const controlledIntensityContent = `It is crucial to keep easy runs extremely easy so you are ready for the next sub threshold session. A good rule of thumb is below 70% max HR.`;
-const simplifiedStructureContent = `There is no need for frequent lactate testing as this approach replaces complex, intense VO2 max and tempo workouts with a consistent repeatable sub threshold pattern.`;
+const controlledIntensityContent = `It is crucial to keep easy runs extremely easy so you are ready for the next sub-threshold session. A good rule of thumb is below 70% max HR.`;
+const simplifiedStructureContent = `There is no need for frequent lactate testing as this approach replaces complex, intense VO2 max and tempo workouts with a consistent repeatable sub-threshold pattern.`;
 const highVolumeLowStrainContent = `Aims to accumulate more training stress sustainably by keeping workout intensity at a controlled state (typically 20-30% of weekly time), which builds a strong aerobic engine.`;
 
 const CorePrinciples = () => {
@@ -13,7 +13,7 @@ const CorePrinciples = () => {
       <div className={styles.core__principles__grid}>
         <div className={styles.core__principles__grid__item}>
           <div className={styles.core__principles__grid__item__title}>
-            Sub Threshold
+            Sub-threshold
           </div>
           <div className={styles.core__principles__grid__item__content}>
             {subThresholdContent}
--- src/components/NorwegianApproachCards/TheScience.tsx
+++ src/components/NorwegianApproachCards/TheScience.tsx
@@ -5,9 +5,9 @@ import CheckMarkList from "../ui/CheckMarkList";
 
 const theScienceList = [
   "The Norwegian style of training was popularized by Marius Bakken in the early 2000s",
-  "The Ingesbrigsten brothers came along and popularized the double threshold method (mostly used by elite athletes)",
-  "To maximize training load and minimize fatigue and injury a Norwegian singles method was created for non-elite athletes",
-  "Research suggests a correlation between training load and your race performance, with sub threshold providing that sweet spot of stress vs recovery",
+  "The Ingebrigtsen brothers came along and popularized the double threshold method (mostly used by elite athletes)",
+  "To maximize training load and minimize fatigue and injury, a Norwegian singles method was created for non-elite athletes",
+  "Research suggests a correlation between training load and your race performance, with sub-threshold providing that sweet spot of stress vs recovery",
   "This method delays the onset of lactic acid build up and expands aerobic capacity which are both crucial for endurance performance",
   "This approach provides up to 50% more training volume, 48hr turnaround between workouts, and neuromusclar preservation",
   "LT2 (88% of VO2 max) is the sweet spot that has been proven to produce similar physiological adaptations of high intensity intervals while allowing for high volume and fast recovery"
